840b40bc8995f2a1d1a95ce5b2171ca69879fcb3
Toavina <toavina.andria@gmail.com>
2016-09-04 12:30:23 +0200

    Added answers in comments for matrix algebra

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -33,33 +33,44 @@ for name,matrix in matrices.iteritems():
 for name,matrix in matrices.iteritems():
     print(name+"'s is a "+str(matrix.shape)+" matrix")
 
+"""
+A's is a (2, 3) matrix
+C's is a (3, 2) matrix
+B's is a (2, 2) matrix
+D's is a (2, 3) matrix
+u's is a (1, 4) matrix
+w's is a (4, 1) matrix
+v's is a (1, 4) matrix
+"""
 
 # ## Exercises 2
 
 # In[75]:
 
 print("u+v equals "+str(u+v))
-
+# u+v equals [[ 9  7 -4  9]]
 
 # In[76]:
 
 print("u-v equals "+str(u-v))
+# u-v equals [[ 3 -3 -2  1]]
 
 
 # In[78]:
 
 print("alpha*u equals "+str(alpha*u))
+# alpha*u equals [[ 36  12 -18  30]]
 
 
 # In[92]:
 
 print("u.v equals "+str(np.inner(u,v)))
-
+# u.v equals [[51]]
 
 # In[93]:
 
 print("||u|| equals "+str(np.sqrt(np.sum(np.power(u,2)))))
-
+# ||u|| equals 8.60232526704
 
 # ## Exercises 3
 
@@ -71,17 +82,29 @@ print("A+C is undefined")
 # In[98]:
 
 print("A-C.T equals \n"+str(A-C.T))
-
+"""
+A-C.T equals
+[[-4 -7 -3]
+ [ 3  6  4]]
+"""
 
 # In[100]:
 
 print("C.T+3*D equals \n"+str(C.T+3*D))
-
+"""
+C.T+3*D equals
+[[14  3  3]
+ [ 2  7  9]]
+ """
 
 # In[101]:
 
 print("B*A equals \n"+str(B*A))
-
+"""
+B*A equals
+[[-1 -5 -1]
+ [ 2  7  4]]
+"""
 
 # In[102]:
 
@@ -95,22 +118,41 @@ print("B*C is undefined")
 
 # In[105]:
 
-print("C*B equals "+str(C*B))
-
+print("C*B equals \n"+str(C*B))
+"""
+C*B equals
+[[ 5 -6]
+ [ 9 -8]
+ [ 6 -6]]
+"""
 
 # In[106]:
 
-print("B**4 equals "+str(B**4))
-
+print("B**4 equals \n"+str(B**4))
+""""
+B**4 equals
+[[ 1 -4]
+ [ 0  1]]
+"""
 
 # In[107]:
 
-print ("A*A.T equals "+str(A*A.T))
-
-
+print ("A*A.T equals \n"+str(A*A.T))
+"""
+A*A.T equals
+[[14 28]
+ [28 69]]
+"""
 # In[108]:
 
-print("D.T*D equals "+str(D.T*D))
+print("D.T*D equals \n"+str(D.T*D))
+
+"""
+D.T*D equals
+[[10 -4  0]
+ [-4  8  8]
+ [ 0  8 10]]
+"""
 
 
 
